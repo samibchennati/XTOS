492ac529faa8e0a45565e0d521c335b81048946b
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-03 18:47:24 -0500

    Update xtos.sh

--- xtos.sh
+++ xtos.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
-#github-action genshdoc
-#
-# @file XTOS
-# @brief Entrance script that launches children scripts for each phase of installation.
 
-# Find the name of the folder the scripts are in
-set -a
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-SCRIPTS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/scripts
-CONFIGS_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"/configs
-set +a
-echo -ne "
--------------------------------------------------------------------------
-                    Automated Arch Linux Installer
--------------------------------------------------------------------------
-                Scripts are in directory named XTOS
-"
-    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
-      source $CONFIGS_DIR/setup.conf
-    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/XTOS/scripts/1-setup.sh )|& tee 1-setup.log
-    if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/XTOS/scripts/2-user.sh )|& tee 2-user.log
-    fi
-    ( arch-chroot /mnt $HOME/XTOS/scripts/3-post-setup.sh )|& tee 3-post-setup.log
-    cp -v *.log /mnt/home/$USERNAME
+pacman -S picom conky i3 alacritty polybar
 
-echo -ne "
--------------------------------------------------------------------------
-                    Automated Arch Linux Installer
--------------------------------------------------------------------------
-                Done - Please Eject Install Media and Reboot
-"
+mkdir /usr/share/backgrounds
+mkdir /home/sami/.config/picom
+mkdir /home/sami/.config/conky
+mkdir /home/sami/.config/i3
+mkdir /home/sami/.config/alacritty
+mkdir /home/sami/.config/polybar
+
+mv conky.conf /home/sami/.config/conky
+mv config /home/sami/.config/i3
+mv alacritty.toml /home/sami/.config/alacritty
+mv config.ini /home/sami/.config/polybar
