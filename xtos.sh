fc76f6deff4473be62f07c9d19ca7cc3396e8ba8
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-06 20:17:03 -0500

    add screenlayout bash script

--- xtos.sh
+++ xtos.sh
@@ -8,10 +8,12 @@ mkdir /home/sami/.config/conky
 mkdir /home/sami/.config/i3
 mkdir /home/sami/.config/alacritty
 mkdir /home/sami/.config/polybar
+mkdir /home/sami/.config/screenlayout
 
 mv picom.conf /home/sami/.config/picom
 mv conky.conf /home/sami/.config/conky
 mv config /home/sami/.config/i3
 mv alacritty.toml /home/sami/.config/alacritty
 mv config.ini /home/sami/.config/polybar
+mv officelayout.sh /home/sami/.config/screenlayout
 
