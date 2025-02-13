4faa1cd4eff41cdc90fb662881d57ebbbf8d7e72
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-13 17:29:42 -0500

    Update xtos.sh
    Comment out AUR Helper

--- xtos.sh
+++ xtos.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 pacman -S --needed git base-devel picom conky i3 alacritty polybar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox git dmenu lxappearance ttf-font-awesome vim nemo feh
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd
-yay install brave-bin
+#git clone https://aur.archlinux.org/yay.git
+#cd yay
+#makepkg -si
+#cd
+#yay install brave-bin
 
 mkdir /usr/share/backgrounds
 mkdir /home/sami/.config/picom
@@ -16,21 +16,21 @@ mkdir /home/sami/.config/polybar
 mkdir /home/sami/.config/screenlayout
 mkdir /home/sami/.themes
 
-mv backgrounds/ /usr/share/
+mv backgrounds/ /usr/share
 mv picom.conf /home/sami/.config/picom
 mv conky.conf /home/sami/.config/conky
 mv config /home/sami/.config/i3
 mv alacritty.toml /home/sami/.config/alacritty
 mv config.ini /home/sami/.config/polybar
 mv officelayout.sh /home/sami/.config/screenlayout
+mv -f lightdm-gtk-greeter.conf /etc/lightdm
 
 feh --bg-scale /usr/share/backgrounds/XT_Wallpaper_001.jpg
 sed -i -e '$a\background = /usr/share/backgrounds/XT_Wallpaper_001.jpg' /etc/lightdm/lightdm-gtk-greeter.conf
 
 git clone https://github.com/dracula/gtk
 mv gtk/ /home/sami/.themes
-cp -r /home/sami/.themes/gtk/ /usr/share/themes/ 
-mv -f lightdm-gtk-greeter.conf /etc/lightdm
+cp -r /home/sami/.themes/gtk/ /usr/share/themes
 systemctl restart lightdm
 
 git clone https://github.com/Coopydood/HyperFluent-GRUB-Theme
