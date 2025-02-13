30ed74acf14d4c0ada251e29288ccf24c47e4472
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-13 16:40:35 -0500

    Update xtos.sh
    - Add AUR Helper
    - Add Brave Browser
    - Add customization to lightdm-gtk-greeter
    - Clones Dracula Theme from github to set as a theme

--- xtos.sh
+++ xtos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pacman -S picom conky i3 alacritty polybar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox git dmenu lxappearance ttf-font-awesome vim nemo feh
+pacman -S --needed git base-devel picom conky i3 alacritty polybar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox git dmenu lxappearance ttf-font-awesome vim nemo feh
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+yay install brave-bin
 
 mkdir /usr/share/backgrounds
 mkdir /home/sami/.config/picom
@@ -9,6 +14,7 @@ mkdir /home/sami/.config/i3
 mkdir /home/sami/.config/alacritty
 mkdir /home/sami/.config/polybar
 mkdir /home/sami/.config/screenlayout
+mkdir /home/sami/.themes
 
 mv backgrounds/ /usr/share/
 mv picom.conf /home/sami/.config/picom
@@ -19,6 +25,12 @@ mv config.ini /home/sami/.config/polybar
 mv officelayout.sh /home/sami/.config/screenlayout
 
 feh --bg-scale /usr/share/backgrounds/XT_Wallpaper_001.jpg
+sed -i -e '$a\background = /usr/share/backgrounds/XT_Wallpaper_001.jpg' /etc/lightdm/lightdm-gtk-greeter.conf
+
+git clone https://github.com/dracula/gtk
+mv gtk/ /home/sami/.themes
+cp -r /home/sami/.themes/gtk/ /usr/share/themes/ 
+mv -f lightdm-gtk-greeter.conf /etc/lightdm
 
 git clone https://github.com/Coopydood/HyperFluent-GRUB-Theme
 cd HyperFluent-GRUB-Theme
@@ -28,3 +40,5 @@ cd
 sed -i -e '$a\GRUB_THEME="/boot/grub/themes/arch/themes.txt"' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
+ 
