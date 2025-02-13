7be51e2af6e340f1befaf671cb518372f57215bd
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-13 14:49:39 -0500

    Customization update
    - Replace nitrogen with feh
    - Add wallpaper folder

--- xtos.sh
+++ xtos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S picom conky i3 alacritty polybar nitrogen lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox git dmenu lxappearance ttf-font-awesome vim nemo
+pacman -S picom conky i3 alacritty polybar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox git dmenu lxappearance ttf-font-awesome vim nemo feh
 
 mkdir /usr/share/backgrounds
 mkdir /home/sami/.config/picom
@@ -10,12 +10,15 @@ mkdir /home/sami/.config/alacritty
 mkdir /home/sami/.config/polybar
 mkdir /home/sami/.config/screenlayout
 
+mv backgrounds/ /usr/share/
 mv picom.conf /home/sami/.config/picom
 mv conky.conf /home/sami/.config/conky
 mv config /home/sami/.config/i3
 mv alacritty.toml /home/sami/.config/alacritty
 mv config.ini /home/sami/.config/polybar
 mv officelayout.sh /home/sami/.config/screenlayout
+
+feh --bg-scale /usr/share/backgrounds/XT_Wallpaper_001.jpg
 
 git clone https://github.com/Coopydood/HyperFluent-GRUB-Theme
 cd HyperFluent-GRUB-Theme
