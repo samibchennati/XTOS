83ff234db4f33e6948a6e1e9bceb90d03bde7b19
Sami Bchennati <68581336+samibchennati@users.noreply.github.com>
2025-02-17 19:48:08 +0000

    User Update
    - Works with any username
    - Adds multiple directories (Documents, Downloads, Pictures, Music, Videos)
    - Continuity fix in the bash script

--- xtos.sh
+++ xtos.sh
@@ -2,29 +2,31 @@
 
 pacman -S --needed git base-devel picom conky i3 alacritty polybar lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit firefox dmenu lxappearance ttf-font-awesome vim nemo feh blueman ibus
 
-mkdir /home/sami/.config/picom
-mkdir /home/sami/.config/conky
-mkdir /home/sami/.config/i3
-mkdir /home/sami/.config/alacritty
-mkdir /home/sami/.config/polybar
-mkdir /home/sami/.config/screenlayout
-mkdir /home/sami/.themes
+mkdir /home/$USER/.config/picom
+mkdir /home/$USER/.config/conky
+mkdir /home/$USER/.config/i3
+mkdir /home/$USER/.config/alacritty
+mkdir /home/$USER/.config/polybar
+mkdir /home/$USER/.config/screenlayout
+mkdir /home/$USER/.themes
+mkdir /home/$USER/Documents
+mkdir /home/$USER/Downloads
+mkdir /home/$USER/Pictures
+mkdir /home/$USER/Music
+mkdir /home/$USER/Videos
 
 mv backgrounds/ /usr/share
-mv picom.conf /home/sami/.config/picom
-mv conky.conf /home/sami/.config/conky
-mv config /home/sami/.config/i3
-mv alacritty.toml /home/sami/.config/alacritty
-mv config.ini /home/sami/.config/polybar
-mv officelayout.sh /home/sami/.config/screenlayout
+mv picom.conf /home/$USER/.config/picom
+mv conky.conf /home/$USER/.config/conky
+mv config /home/$USER/.config/i3
+mv alacritty.toml /home/$USER/.config/alacritty
+mv config.ini /home/$USER/.config/polybar
+mv officelayout.sh /home/$USER/.config/screenlayout
 mv -f lightdm-gtk-greeter.conf /etc/lightdm
 
-feh --bg-scale /usr/share/backgrounds/XT_Wallpaper_001.jpg
-sed -i -e '$a\background = /usr/share/backgrounds/XT_Wallpaper_001.jpg' /etc/lightdm/lightdm-gtk-greeter.conf
-
 git clone https://github.com/dracula/gtk
-mv gtk/ /home/sami/.themes
-cp -r /home/sami/.themes/gtk/ /usr/share/themes
+mv gtk/ /home/$USER/.themes
+cp -r /home/$USER/.themes/gtk/ /usr/share/themes
 
 git clone https://github.com/Coopydood/HyperFluent-GRUB-Theme
 cd HyperFluent-GRUB-Theme
@@ -35,5 +37,6 @@ sed -i -e '$a\GRUB_THEME="/boot/grub/themes/arch/themes.txt"' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
+feh --bg-scale /usr/share/backgrounds/XT_Wallpaper_001.jpg
+sed -i -e '$a\background = /usr/share/backgrounds/XT_Wallpaper_001.jpg' /etc/lightdm/lightdm-gtk-greeter.conf
 systemctl restart lightdm
-
